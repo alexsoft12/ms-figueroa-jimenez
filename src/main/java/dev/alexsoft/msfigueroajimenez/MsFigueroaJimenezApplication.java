809a863715769dd2a6de7a1ad80296fe7242fbf0
Yuler Figueroa <yuleralex1@gmail.com>
2024-05-01 14:25:26 -0500

    initial commit

---
+++ src/main/java/dev/alexsoft/msfigueroajimenez/MsFigueroaJimenezApplication.java
@@ -0,0 +1,13 @@
+package dev.alexsoft.msfigueroajimenez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFigueroaJimenezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsFigueroaJimenezApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/alexsoft/msfigueroajimenez/MsFigueroaJimenezApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexsoft.msfigueroajimenez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFigueroaJimenezApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
